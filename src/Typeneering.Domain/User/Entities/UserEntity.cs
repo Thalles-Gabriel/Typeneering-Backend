1e6faf64ad805d5f4ec5bb051c3ed00b17d79125
Thalles Oliveira <thalles.gbpereira@gmail.com>
2025-05-28 00:01:34 -0300

    Changed entity user to make nickname obligatory

--- src/Typeneering.Domain/User/Entities/UserEntity.cs
+++ src/Typeneering.Domain/User/Entities/UserEntity.cs
@@ -4,12 +4,14 @@ using Typeneering.Domain.Preference.Entities;
 using Typeneering.Domain.Session.Entities;
 
 namespace Typeneering.Domain.User.Entities;
+
 public sealed class UserEntity : IdentityUser<Guid>
 {
     public string? GitHubToken { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset? UpdatedAt { get; set; }
     public new string UserName { get; set; }
+    public new string NormalizedUserName { get; set; }
     public ICollection<SessionEntity> Sessions { get; set; } = [];
     public ICollection<PreferenceEntity> Preferences { get; set; } = [];
 }
