af342a2149c47053b93ae7cd121146fa1e83101a
Thalles Oliveira <thalles.gbpereira@gmail.com>
2025-06-03 19:41:02 -0300

    Changed properties and added corresponding migrations

--- src/Typeneering.Infraestructure/Migrations/20250603223910_General_Adjustments.Designer.cs
+++ src/Typeneering.Infraestructure/Migrations/20250603223910_General_Adjustments.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Typeneering.Infraestructure;
@@ -11,9 +12,11 @@ using Typeneering.Infraestructure;
 namespace Typeneering.Infraestructure.Migrations
 {
     [DbContext(typeof(TypeneeringDbContext))]
-    partial class TypeneeringDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250603223910_General_Adjustments")]
+    partial class General_Adjustments
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Typeneering.Infraestructure/Migrations/20250603223910_General_Adjustments.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Typeneering.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class General_Adjustments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preferences_Name",
+                table: "Preferences",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Preferences_Name",
+                table: "Preferences");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "Users",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
